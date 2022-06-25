9556bc777761b95e02ec4902b9644e4e593623a5
Azhar <kazhikarimova@inbox.ru>
2022-06-25 14:46:48 +0600

    feat: custom start and end icons

--- script.js
+++ script.js
@@ -66,26 +66,69 @@ let data = [
 /*ОТОБРАЗИТЬ ДАТА В КАРТОЧКЕ ПОД ВЕКАЙЛ ЛИСТОМ */
 let vechileShowList = document.querySelector(".vechile_info");
 vechileShowList.onclick = function () {
- console.log(data)
+  console.log(data);
 };
+
+var startIcon = L.icon({
+  iconUrl: "img/red.png",
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 /*Immitation car routes*/
 function createRoute(data) {
-  //TODO: ДАННЫЕ ОТОБРАЗИТЬ НА САЙДБАРЕ
   data.forEach(function (item, index) {
-    L.marker([item.startPoint.lat, item.startPoint.lng]).addTo(map);
     L.Routing.control({
       waypoints: [
         L.latLng(item.startPoint.lat, item.startPoint.lng),
         L.latLng(item.endPoint.lat, item.endPoint.lng),
       ],
+      routeWhileDragging: true,
+      lineOptions: {
+        styles: [{ color: "green", opacity: 1, weight: 5 }],
+      },
+      createMarker: function (i, waypoint, n) {
+        var marker_icon = null;
+        if (i == 0) {
+          // This is the first marker, indicating start
+          marker_icon = L.icon({
+            iconUrl: "img/red.png",
+            iconSize: [20, 29],
+            iconAnchor: [10, 29],
+            popupAnchor: [0, -29],
+          });
+        } else if (i == n - 1) {
+          //This is the last marker indicating destination
+          marker_icon = L.icon({
+            iconUrl: "img/blue.png",
+            iconSize: [20, 29],
+            iconAnchor: [10, 29],
+            popupAnchor: [0, -29],
+          });
+        }
+        return L.marker(waypoint.latLng, {
+          draggable: false,
+          bounceOnAdd: false,
+          bounceOnAddOptions: {
+            duration: 1000,
+            height: 800,
+            function() {
+              bindPopup(myPopup).openOn(map);
+            },
+          },
+          icon: marker_icon,
+        });
+      },
     }).addTo(map);
   });
 }
 //имитация выгрузка маршрутов из сервера
 setTimeout(() => {
   createRoute(data);
-}, 5000);
+}, 3000);
 
 //ДЛЯ ХЕДЕРА функция:  ОТОБРАЗИТЬ МАРШРУТ ОПРЕДЕЛЕННОЙ МАШИНЫ
 /*find specific car route*/
@@ -95,7 +138,6 @@ function findRoute(carID, data) {
 function helperFilter(value, carID) {
   //  TODO: Удалить другие роуты  и оставить нужный
   if (value.carID === carID) {
-    console.log("FOUND");
     L.marker([value.startPoint.lat, value.startPoint.lng]);
     L.Routing.control({
       waypoints: [
