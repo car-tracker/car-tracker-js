dbb736449dddab837461c5bc1a40a72ebb57b48a
Azhar <kazhikarimova@inbox.ru>
2022-06-29 22:43:32 +0600

    feat: added searchin' specific car route

--- script.js
+++ script.js
@@ -4,28 +4,6 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "OSM",
 }).addTo(map);
 var marker = L.marker([51.12820816040039, 71.43041229248047]).addTo(map);
-
-/*---------------------------------------*/
-
-// map.on("click", function (e) {
-//   L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-//   console.log(e);
-//   L.Routing.control({
-//     waypoints: [
-//       L.latLng(51.12820816040039, 71.43041229248047),
-//       L.latLng(e.latlng.lat, e.latlng.lng),
-//     ],
-//   })
-//     .on("routesfound", function (e) {
-//       e.routes[0].coordinates.forEach(function (coord, index) {
-//         setTimeout(function () {
-//           marker.setLatLng([coord.lat, coord.lng]);
-//         }, 100 * index);
-//       });
-//     })
-//     .addTo(map);
-// });
-
 let data = [
   {
     carID: 125,
@@ -37,7 +15,7 @@ let data = [
       lat: 51.12710984444874,
       lng: 71.42223233385684,
     },
-    progress:  'In progress',
+    progress: "In progress",
   },
   {
     carID: 126,
@@ -49,7 +27,7 @@ let data = [
       lat: 51.13077612181946,
       lng: 71.4296181646681,
     },
-    progress:  'Completed'
+    progress: "Completed",
   },
   {
     carID: 127,
@@ -61,7 +39,7 @@ let data = [
       lat: 51.124315017315105,
       lng: 71.43652183297749,
     },
-    progress:  'In progress'
+    progress: "In progress",
   },
   {
     carID: 128,
@@ -70,29 +48,29 @@ let data = [
       lng: 71.43342665985487,
     },
     endPoint: {
-      lat: 51.124315017315100,
+      lat: 51.1243150173151,
       lng: 71.43652183297749,
     },
-    progress:  'In progress'
+    progress: "In progress",
   },
 ];
 
-/*vechile*/
-/*ОТОБРАЗИТЬ ДАТА В КАРТОЧКЕ ПОД ВЕКАЙЛ ЛИСТОМ */
+
 let vechileShowList = document.querySelector(".vechile_info");
 vechileShowList.onclick = function () {
-  console.log(data);
-  var carsInfo=data.map(function(item){
-    return{
-      carID:item.carID,
+  data.map(function (item) {
+    return {
+      carID: item.carID,
       startPoint: item.startPoint.lat,
       endPoint: item.endPoint.lat,
-      progess: item.progress
+      progess: item.progress,
     };
   });
-  data.forEach(cars=>{
-    var div=document.querySelector("#cars")
-    div.innerHTML=div.innerHTML+`
+  data.forEach((cars) => {
+    var div = document.querySelector("#cars");
+    div.innerHTML =
+      div.innerHTML +
+      `
     <div class="row">
     <div class="block">
     <div class="cards">
@@ -109,23 +87,12 @@ vechileShowList.onclick = function () {
        </div>
        </div>
        </div>
-    `
-     })
+    `;
+  });
 };
 
-
-var startIcon = L.icon({
-  iconUrl: "img/red.png",
-  iconSize: [38, 95], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62], // the same for the shadow
-  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-});
-
-/*Immitation car routes*/
 function createRoute(data) {
-  data.forEach(function (item, index) {
+  data.forEach(function (item) {
     L.Routing.control({
       waypoints: [
         L.latLng(item.startPoint.lat, item.startPoint.lng),
@@ -138,7 +105,6 @@ function createRoute(data) {
       createMarker: function (i, waypoint, n) {
         var marker_icon = null;
         if (i == 0) {
-          // This is the first marker, indicating start
           marker_icon = L.icon({
             iconUrl: "img/red.png",
             iconSize: [20, 29],
@@ -146,7 +112,6 @@ function createRoute(data) {
             popupAnchor: [0, -29],
           });
         } else if (i == n - 1) {
-          //This is the last marker indicating destination
           marker_icon = L.icon({
             iconUrl: "img/blue.png",
             iconSize: [20, 29],
@@ -175,33 +140,27 @@ setTimeout(() => {
   createRoute(data);
 }, 3000);
 
-//ДЛЯ ХЕДЕРА функция:  ОТОБРАЗИТЬ МАРШРУТ ОПРЕДЕЛЕННОЙ МАШИНЫ
-/*find specific car route*/
-function findRoute(carID, data) {
-  data.filter((value) => helperFilter(value, carID));
-}
-function helperFilter(value, carID) {
-  //  TODO: Удалить другие роуты  и оставить нужный
-  if (value.carID === carID) {
-    L.marker([value.startPoint.lat, value.startPoint.lng]);
-    L.Routing.control({
-      waypoints: [
-        L.latLng(value.startPoint.lat, value.startPoint.lng),
-        L.latLng(value.endPoint.lat, value.endPoint.lng),
-      ],
-    }).addTo(map);
+let search = document.querySelector("#search");
+
+search.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    findRoute(search.value, data);
   }
+});
+
+function findRoute(carID, data) {
+  data.map((car) => {
+    if (car.carID === Number(carID)) {
+      L.marker([car.startPoint.lat, car.startPoint.lng]);
+      L.Routing.control({
+        waypoints: [
+          L.latLng(car.startPoint.lat, car.startPoint.lng),
+          L.latLng(car.endPoint.lat, car.endPoint.lng),
+        ],
+      }).addTo(map);
+    }
+  });
 }
-
-//ищет определенный маршрут
-setTimeout(() => {
-  findRoute(125, data);
-}, 10000);
-
-//
-
-//TODO:ДОБАВИТЬ ХЕДЕР И ФУНКЦИЮ ПОИСКА МАРШРУТА
-//TODO:В ВЕКАЙЛ ЛИСТЕ ОТОБРАЗИТЬ В КАРТОЧКАХ ВСЕ ДАННЫЕ НА СЕРВЕРЕ О МАШИНЕ ЮЗЕРЕ КМ ВРЕМЯ ДАТА И ТД ТП ЭТИ ЮЗЕРЫ ТАК ЖЕ ДОЛЖНЫ ПОЯВЛЯТЬСЯ НА КAРТЕ (МАРШРУТ)
-//TODO: УБРАТЬ ПРАВУЮ ИНФОРМАТИВНУЮ КАРТУ
-//TODO: ПРИ РИСОВКЕ МАРШРУТА КАРТА ДЕРГАЕТСЯ И ПЕРЕДВИГАЕТСЯ НА КАЖДЫЙ МАРШРУТ, НАДО СДЕЛАТЬ ТАК ЧТОБЫ КАРТА НЕ МЕНЯЛА ПОЗИЦИЮ ПРИ ПЕРВОМ ВХОДЕ/БЕЗДЕЙСТВИИ
 //НА МАРШРУТ ПОВЕСИТЬ ПОПАП С ДАННЫМИ О МАШИНЕ И ЮЗЕРЕ ПРИ КЛИКАНИИ
